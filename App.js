9568f13a062b11f87f87c4ad50ccffa75a1b35c0
Scott Young <syoung@instructure.com>
2020-08-31 14:06:24 -0600

    add state

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [outputText, setOutputText] = useState('Hello')
   return (
     <View style={styles.container}>
-      <Text>Hello</Text>
+      <Text>{ outputText }</Text>
+      <Button title="change-text" onPress={() => setOutputText('Goodbye') }/>
       <StatusBar style="auto" />
     </View>
   );
